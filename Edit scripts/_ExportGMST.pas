38fbce945a285f07af16e4c6eced648a43965b3d
Felix Dekker <felix@fwdekker.com>
2019-01-09 19:10:18 +0100

    Add game settings dump

---
+++ Edit scripts/_ExportGMST.pas
@@ -0,0 +1,62 @@
+unit _ExportGMST;
+
+var
+    outputLines: TStringList;
+
+
+function Initialize: integer;
+begin
+    outputLines := TStringList.Create;
+    outputLines.Add('"Form ID", "Editor ID", "Type", "Value"');
+end;
+
+function Process(e: IInterface): integer;
+begin
+    outputLines.Add(
+        '"' + LowerCase(IntToHex(FormID(e), 8)) + '", ' +
+        '"' + GetEditValue(ElementBySignature(e, 'EDID')) + '", ' +
+        '"' + LetterToType(copy(GetEditValue(ElementBySignature(e, 'EDID')), 1, 1)) + '", ' +
+        '"' + GetEditValue(LastElement(ElementBySignature(e, 'DATA'))) + '"'
+    );
+end;
+
+function Finalize: integer;
+begin
+    if (outputLines.Count > 0) then
+    begin
+        outputLines.SaveToFile('fo76_dump_gmst.csv');
+    end;
+end;
+
+
+function LetterToType(letter: string): string;
+begin
+    if (letter = 'b') then
+    begin
+        Result := 'boolean';
+    end
+    else if (letter = 'f') then
+    begin
+        Result := 'float';
+    end
+    else if (letter = 'i') then
+    begin
+        Result := 'integer';
+    end
+    else if (letter = 's') then
+    begin
+        Result := 'string';
+    end
+    else if (letter = 'u') then
+    begin
+        Result := 'unsigned integer';
+    end
+    else
+    begin
+        AddMessage('<! DUMP ERROR. UNKNOWN TYPE `' + letter + '` !>');
+        Result := '<! DUMP ERROR. UNKNOWN TYPE `' + letter + '` !>';
+    end;
+end;
+
+
+end.
