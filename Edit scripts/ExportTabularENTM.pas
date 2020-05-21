97cfa0135e51bce2b64689a6e6d62e689eb60da0
Felix W. Dekker <felix@fwdekker.com>
2020-05-21 14:28:00 +0200

    Add image path fields to ENTM dump
    Fixes #19.

--- Edit scripts/ExportTabularENTM.pas
+++ Edit scripts/ExportTabularENTM.pas
@@ -12,13 +12,17 @@ function initialize: Integer;
 begin
     ExportTabularENTM_outputLines := TStringList.create();
     ExportTabularENTM_outputLines.add(
-            '"File"'        // Name of the originating ESM
-        + ', "Form ID"'     // Form ID
-        + ', "Editor ID"'   // Editor ID
-        + ', "Name (FULL)"' // Full name
-        + ', "Name (NNAM)"' // Shortened name
-        + ', "Description"' // Description
-        + ', "Keywords"'    // Sorted JSON array of keywords. Each keyword is represented by its editor ID
+            '"File"'                           // Name of the originating ESM
+        + ', "Form ID"'                        // Form ID
+        + ', "Editor ID"'                      // Editor ID
+        + ', "Name (FULL)"'                    // Full name
+        + ', "Name (NNAM)"'                    // Shortened name
+        + ', "Description"'                    // Description
+        + ', "Storefront image path"'          // Path to where images are located
+        + ', "Storefront image preview"'       // File name of preview image
+        + ', "Storefront confirm image list"'  // Sorted JSON array of file names
+        + ', "Keywords"'                       // Sorted JSON array of keywords. Each keyword is represented by its
+                                               // editor ID
     );
 end;
 
@@ -41,6 +45,9 @@ begin
         + escapeCsvString(evBySign(entm, 'FULL')) + ', '
         + escapeCsvString(evBySign(entm, 'NNAM')) + ', '
         + escapeCsvString(evBySign(entm, 'DESC')) + ', '
+        + escapeCsvString(evBySign(entm, 'ETIP')) + ', '
+        + escapeCsvString(evBySign(entm, 'ETDI')) + ', '
+        + escapeCsvString(getFlatChildList(eByName(entm, 'Storefront Confirm Image List'))) + ', '
         + escapeCsvString(getFlatKeywordList(entm))
     );
 end;
