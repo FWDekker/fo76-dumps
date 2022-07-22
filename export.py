c818194c47075162e8355c436b11eac1ae744750
Florine W. Dekker <florine@fwdekker.com>
2022-07-22 20:51:12 +0200

    Reorganise dump script for readability

--- export.py
+++ export.py
@@ -6,36 +6,34 @@ import subprocess
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from types import SimpleNamespace
+from typing import List
 
 import pandas as pd
 
-from config_default import config as cfg_default
 
+def load_config() -> SimpleNamespace:
+    """Load configuration from config files and set globals."""
+    from config_default import config as cfg_default
+    if Path("config.py").exists():
+        from config import config as cfg_user
+    else:
+        cfg_user = {}
 
-# TODO: Move config loading to special function, then add cfg as param to other functions
+    cfg2 = cfg_default | cfg_user
+    cfg2 = (cfg2 | cfg2["windows_settings"]) if cfg2["windows"] else (cfg2 | cfg2["linux_settings"])
 
+    # Version of this script
+    cfg2["script_version"] = "2.6.0"
+    # Path to scripts
+    cfg2["script_root"] = Path("./Edit scripts").resolve()
+    # Path to exported dumps
+    cfg2["dump_root"] = Path(cfg2["script_root"], "dumps")
+    # Path to store SQLite database at
+    cfg2["db_path"] = Path(cfg2["dump_root"], f"fo76-dumps-v{cfg2['script_version']}-v{cfg2['game_version']}.db")
+    # Path to `_done.txt`
+    cfg2["done_path"] = Path(cfg2["dump_root"], "_done.txt")
 
-# Load configuration and set globals
-if Path("config.py").exists():
-    from config import config as cfg_user
-else:
-    cfg_user = {}
-
-cfg = cfg_default | cfg_user
-cfg = (cfg | cfg["windows_settings"]) if cfg["windows"] else (cfg | cfg["linux_settings"])
-
-# Version of this script
-cfg["script_version"] = "2.6.0"
-# Path to scripts
-cfg["script_root"] = Path("./Edit scripts").resolve()
-# Path to exported dumps
-cfg["dump_root"] = Path(cfg["script_root"], "dumps")
-# Path to store SQLite database at
-cfg["db_path"] = Path(cfg["dump_root"], f"fo76-dumps-v{cfg['script_version']}-v{cfg['game_version']}.db")
-# Path to `_done.txt`
-cfg["done_path"] = Path(cfg["dump_root"], "_done.txt")
-
-cfg = SimpleNamespace(**cfg)
+    return SimpleNamespace(**cfg2)
 
 
 def prompt_confirmation(message: str) -> bool:
@@ -51,21 +49,34 @@ def prompt_confirmation(message: str) -> bool:
             continue
 
 
-def run_executable(command, steam_path, proton_path, compatdata_path):
+def run_executable(command: str, compatdata_path: str):
     """Runs the executable with parameters defined in `command`. On Windows, the command is executed normally. On Linux,
-    the command is executed in a Proton instance defined by `steam_path` and `proton_path`, using `compatdata_path`."""
+    the command is executed in a Proton instance using `compatdata_path`."""
     if cfg.windows:
         os.system(command)
     else:
         os.system(
-            f"STEAM_COMPAT_CLIENT_INSTALL_PATH='{steam_path}' "
+            f"STEAM_COMPAT_CLIENT_INSTALL_PATH='{cfg.steam_path}' "
             f"STEAM_COMPAT_DATA_PATH='{compatdata_path}' "
-            f"'{proton_path}' run {command} "
+            f"'{cfg.proton_path}' run {command} "
             f">/dev/null"
         )
 
 
-def run_xedit():
+def concat_parts_of(input_paths: List[str], output_path: str):
+    """Concatenates the contents of all files in `input_paths` and writes the output to `output_path`."""
+    if len(input_paths) == 0:
+        return
+
+    input_paths.sort()
+
+    with open(output_path, "wb") as f_out:
+        for input_path in input_paths:
+            with open(input_path, "rb") as f_in:
+                shutil.copyfileobj(f_in, f_out)
+
+
+def xedit():
     """Runs xEdit and waits until it closes.
 
     Tries to detect whether the script ran successfully by checking if `_done.txt` was created or modified."""
@@ -75,7 +86,7 @@ def run_xedit():
     if Path(cfg.done_path).exists():
         if not prompt_confirmation("WARNING: '_done.txt' already exists, indicating a dump already exists in the "
                                    "target folder. Continue anyway? (y/n) "):
-            exit()
+            return
         os.remove(cfg.done_path)
 
     # Create ini if it does not exist
@@ -91,23 +102,28 @@ def run_xedit():
     # Actually run xEdit
     cwd = os.getcwd()
     os.chdir(cfg.script_root)
-    run_executable(f"'{cfg.xedit_path}' ExportAll.fo76pas", cfg.steam_path, cfg.proton_path, cfg.xedit_compatdata_path)
+    run_executable(f"'{cfg.xedit_path}' ExportAll.fo76pas", cfg.xedit_compatdata_path)
     os.chdir(cwd)
 
     # Check if `_done.txt` changed
     new_done_time = done_file.stat().st_mtime if done_file.exists() else None
     if new_done_time is None or done_time == new_done_time:
         if not prompt_confirmation("WARNING: xEdit did not create or update '_done.txt'. Continue anyway? (y/n) "):
-            exit()
+            return
+
+    # Post-processing
+    xedit_prefix_outputs()
+    xedit_concat_parts()
+    xedit_create_db()
 
     print("> Done running xEdit.\n")
 
 
-def prefix_files():
+def xedit_prefix_outputs():
     """Renames the exported files so that they have a prefix `tabular.` or `wiki.` depending on the file type.
 
     Files that already have the appropriate prefix are unaffected."""
-    print("> Prefixing files.")
+    print(">> Prefixing files.")
 
     for filename in glob.glob(f"{cfg.dump_root}/*.csv") + glob.glob(f"{cfg.dump_root}/*.wiki"):
         path = Path(filename)
@@ -119,44 +135,31 @@ def prefix_files():
 
         path.rename(Path(path.parent, f"{prefix}{path.name}"))
 
-    print("> Done prefixing files.\n")
+    print(">> Done prefixing files.\n")
 
 
-def concat_parts_of(input_paths, output_path):
-    """Concatenates the contents of all files in `input_paths` and writes the output to `output_path`."""
-    if len(input_paths) == 0:
-        return
-
-    input_paths.sort()
-
-    with open(output_path, "wb") as f_out:
-        for input_path in input_paths:
-            with open(input_path, "rb") as f_in:
-                shutil.copyfileobj(f_in, f_out)
-
-
-def concat_parts():
+def xedit_concat_parts():
     """Concatenates files that have been dumped in parts by the xEdit script."""
-    print("> Combining dumped CSV parts.")
+    print(">> Combining dumped CSV parts.")
 
-    print(">> Combining 'tabular.IDs.csv'.")
+    print(">>> Combining 'tabular.IDs.csv'.")
     concat_parts_of(glob.glob(f"{cfg.dump_root}/IDs.csv.*"), f"{cfg.dump_root}/tabular.IDs.csv")
 
-    print(">> Combining 'wiki.TERM.wiki'.")
+    print(">>> Combining 'wiki.TERM.wiki'.")
     concat_parts_of(glob.glob(f"{cfg.dump_root}/TERM.wiki.*"), f"{cfg.dump_root}/wiki.TERM.wiki")
 
-    print("> Done combining dumped CSV parts.\n")
+    print(">> Done combining dumped CSV parts.\n")
 
 
-def import_in_sqlite():
+def xedit_create_db():
     """Imports the dumped CSVs into an SQLite database."""
-    print(f"> Importing CSVs into SQLite database at '{cfg.db_path}'.")
+    print(f">> Importing CSVs into SQLite database at '{cfg.db_path}'.")
 
     # Check for existing files
     if Path(cfg.db_path).exists():
         if not prompt_confirmation(f"WARNING: '{Path(cfg.db_path).name}' already exists and will be deleted. Continue? "
                                    f"(y/n) "):
-            exit()
+            return
         os.remove(cfg.db_path)
 
     # Import into database
@@ -170,10 +173,10 @@ def import_in_sqlite():
             df.columns = df.columns.str.replace(" ", "_")
             df.to_sql(table_name, con, index=False)
 
-    print(f"> Done importing CSVs into SQLite database at '{cfg.db_path}'.\n")
+    print(f">> Done importing CSVs into SQLite database at '{cfg.db_path}'.\n")
 
 
-def run_ba2extract():
+def ba2extract():
     """Creates raw dumps using ba2extract."""
     print("> Extracting Bethesda archives.")
     temp_dir = TemporaryDirectory(prefix="fo76-dumps-")
@@ -187,8 +190,7 @@ def run_ba2extract():
 
     # Extract archives
     targets_string = " ".join([f"'{cfg.game_root}/Data/{it}'" for it in cfg.ba2extract_archives])
-    run_executable(f"'{cfg.ba2extract_path}' {targets_string} '{temp_dir.name}'",
-                   cfg.steam_path, cfg.proton_path, cfg.ba2extract_compatdata_path)
+    run_executable(f"'{cfg.ba2extract_path}' {targets_string} '{temp_dir.name}'", cfg.ba2extract_compatdata_path)
 
     # Move extracted files
     for archive_path, desired_path in cfg.ba2extract_files.items():
@@ -197,7 +199,7 @@ def run_ba2extract():
     print("> Done extracting Bethesda archives.\n")
 
 
-def archive_files():
+def archive_dumps():
     """Archives all `.csv`, `.wiki`, and `.db` dumps that are larger than 10MB."""
     print("> Archiving dumps.")
     cwd = os.getcwd()
@@ -236,13 +238,18 @@ def archive_files():
     print("> Done archiving dumps.\n")
 
 
-if __name__ == "__main__":
+def main():
+    """The main function."""
     print(f"Creating fo76-dumps using '{cfg.xedit_path}'.")
     if cfg.game_version == "x.y.z.w":
         if not prompt_confirmation("WARNING: The game version is set to 'x.y.z.w' in the configuration, which is "
                                    "probably incorrect. If you continue, some dumps will have incorrect names. Do you "
                                    "want to continue anyway? (y/n) "):
             exit()
+    if Path.exists(cfg.dump_root) and len(os.listdir(cfg.dump_root)) != 0:
+        if prompt_confirmation("INFO: The dump output directory exists and is not empty. Do you want to remove the "
+                               "directory and its contents? This is optional. (y/n) "):
+            shutil.rmtree(cfg.dump_root)
     print("")
 
     # Create dumps output dir, just in case
@@ -250,17 +257,22 @@ if __name__ == "__main__":
 
     # xEdit
     if cfg.enable_xedit:
-        run_xedit()
-        prefix_files()
-        concat_parts()
-        import_in_sqlite()
+        xedit()
+        xedit_prefix_outputs()
+        xedit_concat_parts()
+        xedit_create_db()
 
     # ba2extract
     if cfg.enable_ba2extract:
-        run_ba2extract()
+        ba2extract()
 
     # Archival
     if cfg.enable_archive_large or cfg.enable_archive_esms:
-        archive_files()
+        archive_dumps()
 
     print("Done!")
+
+
+if __name__ == "__main__":
+    cfg = load_config()
+    main()
